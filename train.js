ffc9dadf5cf6a4accdbb03c061a9b77ef5e447eb
mahmud1995 <makhmudgo95@gmail.com>
2024-08-20 23:44:29 +0900

    feat: A-TASK Bajarildi

--- train.js
+++ train.js
@@ -73,33 +73,64 @@
 
 /*                                  Promise functions*/
 
-console.log("Jack Ma Maslahatlari");
+// console.log("Jack Ma Maslahatlari");
 
-const list = [
-    "yaxshi talaba bo'ling", // 0-20
-    "to'g'ri bosjliq tanlang va ko'proq xato qiling", // 20-30
-    "uzingizga ushlashingizni boshlang", // 30-40
-    "siz kuchli bo'lgan narsalarni qiling", // 40-50
-    "yoshlarga investitsiya qiling", //
-    "endi dam oling, foydasi yo'q endi", // 60
-];
+// const list = [
+//     "yaxshi talaba bo'ling", // 0-20
+//     "to'g'ri bosjliq tanlang va ko'proq xato qiling", // 20-30
+//     "uzingizga ushlashingizni boshlang", // 30-40
+//     "siz kuchli bo'lgan narsalarni qiling", // 40-50
+//     "yoshlarga investitsiya qiling", //
+//     "endi dam oling, foydasi yo'q endi", // 60
+// ];
 
-async function maslahatBering(a) {
-    if(typeof a !== "number") throw new Error("insert a number", null);
-    else if(a <= 20) return list[0];
-    else if(a > 20 && a <= 30) return(null, list[1]);
-    else if(a > 30 && a <= 40) return(null, list[2]);
-    else if(a > 40 && a <= 50) return(null, list[3]);
-    else if(a > 50 && a <= 60) return(null, list[4]);
-    else {
-            // setTimeout(function () {
-            //     callback(null, list[5]);
-            // }, 5000);
-            // return list[5];
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(list[5]);
-            }, 5000);
-        });
+// async function maslahatBering(a) {
+//     if(typeof a !== "number") throw new Error("insert a number", null);
+//     else if(a <= 20) return list[0];
+//     else if(a > 20 && a <= 30) return(null, list[1]);
+//     else if(a > 30 && a <= 40) return(null, list[2]);
+//     else if(a > 40 && a <= 50) return(null, list[3]);
+//     else if(a > 50 && a <= 60) return(null, list[4]);
+//     else {
+//             // setTimeout(function () {
+//             //     callback(null, list[5]);
+//             // }, 5000);
+//             // return list[5];
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve(list[5]);
+//             }, 5000);
+//         });
+//     }
+// };
+
+// // call via asynch
+// async function run() {
+//     let javob = await maslahatBering(34);
+//     console.log(javob);
+//     javob = await maslahatBering(62);
+//     console.log(javob);
+//     javob = await maslahatBering(23);
+//     console.log(javob);
+// }
+// run();
+
+
+////////////////////////////////////////////////////////////////////////////////////////////  A-TASK   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let count_soz = 0;
+function topHarf(soz, harf) {
+    // let count_soz = 0;
+    for (let position = 0;position < harf.length; position++) {
+        if (harf.charAt(position) == soz) {
+            count_soz += 1;
+        }
     }
-};
+    return count_soz;
+}
+
+// Variant 1
+gucci = topHarf("c", "guccci");
+console.log("Gucci da c nechta?", gucci);
+//// Variant 2
+// console.log(topHarf("e","enginer"));
