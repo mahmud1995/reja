e3ce41d329c987b86095fcb386d961b1323d8bd9
mahmud1995 <makhmudgo95@gmail.com>
2024-08-26 08:15:38 +0900

    feat: C-TASK

--- public/browser.js
+++ public/browser.js
@@ -1,8 +1,7 @@
 console.log("FrontEnd JS ishga tushdi");
 
 function itemTemplate(item) {
-    return `
-  <li
+    return `<li
           class="list-group-item list-group-item-info d-flex align-items-center justify-content-between"
         >
           <span class="item-text">${item.reja} </span>
@@ -13,15 +12,16 @@ function itemTemplate(item) {
             >
               O'zgartirish
             </button>
+            
             <button
             data-id="${item._id}"
-              class="edit-me btn btn-danger btn-sm"
+              class="delete-me btn btn-danger btn-sm"
             >
               O'chirish
             </button>
           </div>
         </li>
-  `;
+    `;
 }
 let createField = document.getElementById("create-field");
 console.log("send data front to backend")
@@ -42,21 +42,14 @@ document.getElementById("create-form").addEventListener("submit", function(e) {
     });
 });
 
-// let createField = document.getElementById("create-field");
-// console.log("A1: send data from front to backend");
-// document.getElementById("create-form").addEventListener("submit", function (e) {
-//   e.preventDefault();
-
-//   axios
-//     .post("/create-item", { reja: createField.value })
-//     .then((response) => {
-//       console.log("A5: recieving  data from backend to frontend");
-
-//       document
-//         .getElementById("item-list")
-//         .insertAdjacentHTML("beforeend", itemTemplate(response.data));
-//       createField.value = "";
-//       createField.focus();
-//     })
-//     .catch((err) => console.log("Could get data"), err);
-// });
+document.addEventListener("click", function (e) {
+    // delete oper
+    if (e.target.classList.contains("delet-me")) {
+        if (confirm("Aniq ochirmoqchimisiz?")) {
+            alert("Yes deb javob berildi");
+        } else {
+            alert("NO deb jabob berildi")
+        }
+    }
+    // edit oper
+});
--- train.js
+++ train.js
@@ -1,17 +1,45 @@
-// B-TASK STARTS
-function countDigits(string) {
+// Task B:
+//Shunday function tuzing, u 1ta string parametrga ega bolsin,
+//hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+//MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+
+function count_digits(str) {
   let count = 0;
-  for (let n of string) {
-    if(n >= "0" && n <= "9"){
-      count++; 
+  for (let n of str) {
+    if (n >= "0" && n <= "9") {
+      count++;
     }
   }
   return count;
 }
+
+const result = count_digits("ah98mad7al3ie1v");
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+// B-TASK STARTS
+// function countDigits(string) {
+//   let count = 0;
+//   for (let n of string) {
+//     if(n >= "0" && n <= "9"){
+//       count++; 
+//     }
+//   }
+//   return count;
+// }
  
-const natija = countDigits("fsidfi324fasdfa3ini3n");
-console.log(natija);
-// B-TASK ENDS
+// const natija = countDigits("fsidfi324fasdfa3ini3n");
+// console.log(natija);
+// // B-TASK ENDS
 
 // --------------------------------------------------------
 
