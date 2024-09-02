9f49351d495bb368c70297e10489d316d543ccad
mahmud1995 <makhmudgo95@gmail.com>
2024-09-02 10:00:27 +0900

    feat F-TASK is DONE

--- train.js
+++ train.js
@@ -1,3 +1,38 @@
+/*F-TASK: 
+
+Shunday findDoublers function tuzing, 
+unga faqat bitta string argument pass bolib, 
+agar stringda bir hil harf qatnashgan bolsa true, 
+qatnashmasa false qaytarishi kerak.
+MASALAN: getReverse("hello") return true return qiladi
+
+*/
+
+const getDoublers = (str) => {
+  // convert the string to array
+  const arr = str.split("");
+  const new_arr = [...new Set(arr)];
+
+  if (arr.length !== new_arr.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+const result = getDoublers("hello");
+console.log("result:", result);
+
+
+
+
+
+
+
+
+
+
+
+
 /*E-TASK: 
 
 Shunday function tuzing, 
@@ -8,19 +43,19 @@ MASALAN: getReverse("hello") return qilsin "olleh"
 @MITASK
 */
 
-function get_reverse(str) {
-  // empty string
-  let new_string = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    new_string += str[i];
-  }
-  return new_string;
-}
-const result = get_reverse("hello");
-console.log("result:", result);
+// function get_reverse(str) {
+//   // empty string
+//   let new_string = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     new_string += str[i];
+//   }
+//   return new_string;
+// }
+// const result = get_reverse("hello");
+// console.log("result:", result);
 
-const result2 = get_reverse("koobkrow");
-console.log("result2:", result2);
+// const result2 = get_reverse("koobkrow");
+// console.log("result2:", result2);
 
 
 
@@ -50,71 +85,71 @@ console.log("result2:", result2);
 // Masalan: const shop = new Shop(4,5,2) shop.qoldiq() return hozir 20:40 4ta non, 5ta lagmon
 // va 2ta cola mavjud! shop.sotish("non", 3) & shop.qabul("cola", 4) & shop.qoldiq() return hozir 20:50 ta 1ta non va 5 ta lagmon va 6ta cola mavjud
 
-let now = new Date();
-let hour = now.getHours();
-let minute = now.getMinutes();
-minute = minute < 10 ? "0" + minute : minute;
+// let now = new Date();
+// let hour = now.getHours();
+// let minute = now.getMinutes();
+// minute = minute < 10 ? "0" + minute : minute;
 
 
-class Shop {
-  constructor(lagmon, non, cola) {
-    this.lagmon = lagmon;
-    this.non = non;
-    this.cola = cola;
-  }
+// class Shop {
+//   constructor(lagmon, non, cola) {
+//     this.lagmon = lagmon;
+//     this.non = non;
+//     this.cola = cola;
+//   }
 
-  sotish(items, amount) {
-    // lag'mon
-    if (items === "lagmon") {
-      if (this.lagmon >= amount) {
-        this.lagmon -= amount;
-      }
-    }
+//   sotish(items, amount) {
+//     // lag'mon
+//     if (items === "lagmon") {
+//       if (this.lagmon >= amount) {
+//         this.lagmon -= amount;
+//       }
+//     }
 
-    // non
-    if (items === "non") {
-      if (this.non >= amount) {
-        this.non -= amount;
-      }
-    }
+//     // non
+//     if (items === "non") {
+//       if (this.non >= amount) {
+//         this.non -= amount;
+//       }
+//     }
 
-    // cola
+//     // cola
 
-    if (items === "cola") {
-      if (this.cola >= amount) {
-        this.cola -= amount;
-      }
-    }
+//     if (items === "cola") {
+//       if (this.cola >= amount) {
+//         this.cola -= amount;
+//       }
+//     }
 
-    return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola bor holos!`;
-  }
+//     return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola bor holos!`;
+//   }
 
-  qabul(items, amount) {
-    // lagmon
-    if (items === "lagmon") {
-      this.lagmon += amount;
-    }
+//   qabul(items, amount) {
+//     // lagmon
+//     if (items === "lagmon") {
+//       this.lagmon += amount;
+//     }
 
-    // non
-    if (items === "non") {
-      this.non += amount;
-    }
+//     // non
+//     if (items === "non") {
+//       this.non += amount;
+//     }
 
-    // cola
-    if (items === "cola") {
-      this.cola += amount;
-    }
-    return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola bor holos!`;
-  }
+//     // cola
+//     if (items === "cola") {
+//       this.cola += amount;
+//     }
+//     return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola bor holos!`;
+//   }
 
-  qoldiq() {
-    return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola qolgan!`;
-  }
-}
+//   qoldiq() {
+//     return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola qolgan!`;
+//   }
+// }
 
-const trade = new Shop(6, 2, 4);
-console.log(trade.sotish("cola", 2));
-console.log(trade.qabul("non", 2));
+// const trade = new Shop(6, 2, 4);
+// console.log(trade.sotish("cola", 2));
+// console.log(trade.qabul("non", 2));
 
 
 
