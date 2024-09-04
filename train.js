51d9eb065ef96af126250af2db84322d364f7ab4
mahmud1995 <makhmudgo95@gmail.com>
2024-09-04 15:15:21 +0900

    feat: G-TASK Is Done

--- train.js
+++ train.js
@@ -1,3 +1,39 @@
+/*              G-TASK: 
+
+Shunday function tuzingki unga integerlardan iborat
+ array pass bolsin va function bizga osha arrayning 
+ eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
+*/
+
+function getHighestIndex(arr) {
+  // defining new variable
+  let greatest;
+  let greatestIndex;
+  // looping through the arr parameter of function
+  for (let i = 0; i < arr.length; i++) {
+    // giving the condition if a found number is not the greatest
+    // in that case arr[i] is greater than that.
+    if (!greatest || arr[i] > greatest) {
+      greatest = arr[i];
+      // if not the greatestIndex = looped array of i
+      greatestIndex = i;
+    }
+  }
+  return greatestIndex;
+}
+let array = [5, 21, 12, 21, 8];
+const result = getHighestIndex(array);
+console.log("result:", result);
+
+
+
+
+
+
+
+
+
 /*F-TASK: 
 
 Shunday findDoublers function tuzing, 
@@ -8,19 +44,19 @@ MASALAN: getReverse("hello") return true return qiladi
 
 */
 
-const getDoublers = (str) => {
-  // convert the string to array
-  const arr = str.split("");
-  const new_arr = [...new Set(arr)];
+// const getDoublers = (str) => {
+//   // convert the string to array
+//   const arr = str.split("");
+//   const new_arr = [...new Set(arr)];
 
-  if (arr.length !== new_arr.length) {
-    return true;
-  } else {
-    return false;
-  }
-};
-const result = getDoublers("hello");
-console.log("result:", result);
+//   if (arr.length !== new_arr.length) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+// const result = getDoublers("hello");
+// console.log("result:", result);
 
 
 
